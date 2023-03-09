e432beed7753cfb95331d503a2435dfa9aed7384
Brandon Smith <smithb@wlwv.k12.or.us>
2023-03-09 08:30:35 -0800

    initial commit

---
+++ src/ArrayListFun.java
@@ -0,0 +1,34 @@
+//import ArrayLists
+
+
+public class ArrayListFun {
+    public static void main(String[] args) {
+        //create an ArrayList of Integers
+        //format is ArrayList<E> listName = new ArrayList<E>();
+
+        //create an ArrayList of Doubles
+
+        //create an ArrayList of Strings
+
+
+        //print your list of Doubles
+
+
+        //add a new value to the end list of Integers then print it
+
+        //add a new value at a specific index in the Integer then print it
+
+
+        //get the item an index 4 in the Integer array list
+
+        //add a new value to the Integer array list and get its size
+
+        //set the item at index 3 to be 42.0 in the Doubles array, store that element, then
+        // print the new list and the replaced value
+
+        //remove the item at index 2 in the Doubles array
+
+
+
+    }
+}
